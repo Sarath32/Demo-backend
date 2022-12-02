391f79381d6a4a0c33fc0daf54829820da4bc76e
Sarath32 <sarathraja01@gmail.com>
2022-12-02 18:40:32 +0530

    updated

--- src/Interview_String01/CountNumofVowels12.java
+++ src/Interview_String01/CountNumofVowels12.java
@@ -4,7 +4,7 @@ public class CountNumofVowels12 {
 
 	public static void main(String[] args) {
 
-		String a="Full Stack Developer";
+		String a="Full StackDeveloper";
 		a=a.toLowerCase();
 		char s[]=a.toCharArray();
 		int vcount=0;
